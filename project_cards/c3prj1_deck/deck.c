e73afeac5bd23be217949f96f2c1576a35b01297
Sinopse <aleksander.tarasov9l@gmail.com>
2020-12-29 15:03:44 +0100

    deck.c working on deck_contains

--- project_cards/c3prj1_deck/deck.c
+++ project_cards/c3prj1_deck/deck.c
@@ -4,9 +4,18 @@
 #include "deck.h"
 void print_hand(deck_t * hand){
 
+  for (size_t index = 0; index < 6; index++) {
+    card_t c = hand[index];
+    print_card(c);
+  }
+  
 }
 
+// check if a deck contains a particular card
 int deck_contains(deck_t * d, card_t c) {
+
+  
+  
   return 0;
 }
 

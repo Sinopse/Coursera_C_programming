f44d6827a713f057f1300517bb482e8684c37ba3
Sinopse <aleksander.tarasov9l@gmail.com>
2020-12-30 21:05:37 +0100

    added a test case for checking cards in deck

--- project_cards/c3prj1_deck/deck.c
+++ project_cards/c3prj1_deck/deck.c
@@ -13,7 +13,20 @@ void print_hand(deck_t * hand){
 
 // check if a deck contains a particular card
 int deck_contains(deck_t * d, card_t c) {
+  char val = c.value;
+  char suit = c.suit;
+  const char card[3] = {val, suit, '\0'};  // contains value and suit of a card
 
+  // comparison by moving from char to char via pointers
+  char *pcard = card;
+
+  for (size_t index = 0; index < n_cards; index++) {
+    char ***card_from_deck = &d.cards;
+    char *p = *card_from_deck;	// value and suit of the comparison
+
+    while (*p != '\0') {
+      if (*pcard == 
+  
   
   
   return 0;
